156be8d3bdcc8b2bf2230a9bdf8d72ca3c074af8
Hemant Kumar <gethemant@gmail.com>
2016-12-03 08:16:36 -0500

    Extract code for ansible playbook in its own file

---
+++ ansible-playbook/bootstrap.sh
@@ -0,0 +1,8 @@
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+mv /etc/ansible/hosts /etc/ansible/hosts.orig
+cat > /etc/ansible/hosts <<EOF
+localhost ansible_connection=local
+EOF
+ansible-playbook -s basic.yaml
