fc2db0a9848d3789c1a8fbb21d271defd1d5262c
Hemant Kumar <gethemant@gmail.com>
2016-12-03 23:28:57 -0500

    Cloud config. has started to come together

--- ansible-playbook/bootstrap.sh
+++ ansible-playbook/bootstrap.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+shopt -s extglob
+
 apt-get install -y software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible
 mv /etc/ansible/hosts /etc/ansible/hosts.orig
 mkdir -p /etc/ansible/facts.d/
-cp *.fact /etc/ansible/facts.d/
+
+FILES=("./../"*.fact)
+for f in "${FILES[@]}"
+do
+    cp "${f}" /etc/ansible/facts.d/.
+done
+
 cat > /etc/ansible/hosts <<EOF
 localhost ansible_connection=local
 EOF
