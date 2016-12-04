f5da7336c42c436bc70d21e690927b3ab8211e46
Hemant Kumar <gethemant@gmail.com>
2016-12-03 21:47:05 -0500

    Add new files

--- create_cluster.sh
+++
@@ -1,6 +0,0 @@
-server=$@
-
-ssh -i "${HOME}/.ssh/google_compute_engine" "ubuntu@${server}" "mkdir -p ~/gce_invoker"
-rsync -Pav -e "ssh -i ${HOME}/.ssh/google_compute_engine" . "ubuntu@${server}:gce_invoker/"
-ssh -i "${HOME}/.ssh/google_compute_engine" "ubuntu@${server}" "cd ~/gce_invoker/ansible-playbook && sudo ./bootstrap.sh"
-ssh -i "${HOME}/.ssh/google_compute_engine" "ubuntu@${server}" "cd ~/gce_invoker/ansible-playbook && ansible-playbook -s kube.yaml"
