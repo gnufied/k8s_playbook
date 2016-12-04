818747b20a01e59da26fe8409cd29df13e77fb77
Hemant Kumar <gethemant@gmail.com>
2016-12-03 22:46:15 -0500

    Ignore facts automatically

--- ansible-playbook/bootstrap.sh
+++ ansible-playbook/bootstrap.sh
@@ -3,6 +3,8 @@ apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible
 mv /etc/ansible/hosts /etc/ansible/hosts.orig
+mkdir -p /etc/ansible/facts.d/
+cp *.fact /etc/ansible/facts.d/
 cat > /etc/ansible/hosts <<EOF
 localhost ansible_connection=local
 EOF
